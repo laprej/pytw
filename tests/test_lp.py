50ec8e3f288f3137e8e05abfb968d973f9e76456
Justin M. LaPre <justin@lapre.com>
2024-05-11 21:43:15 -0400

    add test_lp

---
+++ tests/test_lp.py
@@ -0,0 +1,26 @@
+import random
+from sys import float_info
+
+import pytest
+
+from pytw.event import Event
+from pytw.logical_process import LogicalProcess
+
+
+@pytest.fixture
+def LP():
+    lp = LogicalProcess()
+    for _ in range(3):
+        e = Event(ts=random.random())
+        lp.add_event(e)
+    return lp
+
+
+def test_next(LP):
+    prev = 0.0
+
+    for _ in range(3):
+        e = LP.next_event()
+        cur = e.timestamp
+        assert cur >= prev
+        prev = cur
