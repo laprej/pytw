c3cdb3efb106461bf104ebdc16bdc24cd89e8ce3
Justin M. LaPre <justin@lapre.com>
2024-07-30 14:37:55 +0000

    move PE fixture

--- tests/test_pe.py
+++ tests/test_pe.py
@@ -1,9 +1,26 @@
+from random import random
+
 import pytest
 
+from pytw.event import Event
+from pytw.logical_process import LogicalProcess
+from pytw.mpi import MPIBase
 from pytw.processing_element import ProcessingElement as PE
+from pytw.sequential import Sequential
 
 
 @pytest.fixture
 def PE():
-    pe = PE()
-    return pe
+    engine = MPIBase(PE)
+    pe = Sequential(engine)
+
+    lp_count = 16
+    element_count = 10
+    for _ in range(lp_count):
+        lp = LogicalProcess()
+        for _ in range(element_count):
+            e = Event(ts=random())
+            lp.add_event(e)
+        pe.lps.append(lp)
+
+    yield pe
